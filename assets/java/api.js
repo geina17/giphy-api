4cabb57bde24e49d6df64a3e7ea983b4ad82658e
geina corrente <33171782+geina17@users.noreply.github.com>
2018-02-25 17:40:47 -0700

    Add files via upload

---
+++ assets/java/api.js
@@ -0,0 +1,9 @@
+//need array of strings
+var queens =["Rue Paul","Drag Race","Yas","Drag Queen","Sasha Velor","Shade","Bye Felica"];
+//save var to topics
+//create bttns in html
+	//use loop to append a bttn for each string
+//when clicked grab 10 static (non animated gif) frm gify api
+//when hovers of static image>image plays
+//keep it pg
+//add a form so user can add to topics
